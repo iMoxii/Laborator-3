fca006f2300f98fe0f8a7d778b06a4ed992c7812
iMoxii <92295177+iMoxii@users.noreply.github.com>
2021-11-01 10:50:56 +0200

    Add files via upload

---
+++ locatie.js
@@ -0,0 +1,28 @@
+get_geo_position();
+
+function on_success(position){
+  document.getElementById("latitude").innerHTML= "Latitude= "+position.coords.latitude;
+  document.getElementById("longitude").innerHTML= "Longitude= "+position.coords.longitude;
+  document.getElementById("acc").innerHTML= "Accuracy= "+position.coords.accuracy;
+  document.getElementById("altitude").innerHTML= "Altitude= "+position.coords.altitude;
+
+  var latlon= position.coords.latitude+ ","+ position.coords.longitude;
+  var img_url= "https://maps.googleapis.com/maps/api/staticmap?center="+latlon+"&zoom=14&size=400x300&key=AIzaSyD0B-EY48LTU6OJzvbPw-pqpLk-4thIfus";
+
+  document.getElementById("map").innerHTML= "<img src= '"+img_url+"'>";
+
+  function_on_error(e){
+    document.getElementById("text").innerHTML= e.message;
+  }
+
+  var geo_params={
+    enableHighAccuracy: true,
+    timeout: 1000,
+    maximumAge:0
+  };
+
+  function get_geo_position() {
+    var geo= navigator.geolocation;
+    geo.getCurrentPosition(on_success, on_error, geo_params)
+  }
+}
